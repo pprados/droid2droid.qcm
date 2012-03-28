8f0981dbd899c4e97c9e33e715b99c8bc7a682b9
Hugues Eddy HODONOU <eddyhugues@gmail.com>
2012-03-28 10:07:26 +0200

    First Commit

---
+++ src/org/remoteandroid/apps/qcm/QCMActivity.java
@@ -0,0 +1,13 @@
+package org.remoteandroid.apps.qcm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QCMActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
