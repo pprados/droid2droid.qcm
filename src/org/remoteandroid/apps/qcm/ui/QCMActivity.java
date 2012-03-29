c2140e3ea9991c0372610d534e7c6af81f7e6d91
Hugues Eddy HODONOU <eddyhugues@gmail.com>
2012-03-29 12:01:21 +0200

    Improvement of the design

--- src/org/remoteandroid/apps/qcm/ui/QCMActivity.java
+++ src/org/remoteandroid/apps/qcm/ui/QCMActivity.java
@@ -1,16 +1,55 @@
 package org.remoteandroid.apps.qcm.ui;
 
-
+import org.remoteandroid.RemoteAndroidManager;
 import org.remoteandroid.apps.qcm.R;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
-public class QCMActivity extends SherlockActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class QCMActivity extends SherlockActivity
+{
+	private static final int 	REQUEST_CONNECT_CODE=1;
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.main_activity, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId()) 
+	    {
+		    case R.id.menu_connect:
+				Intent intent=new Intent(RemoteAndroidManager.ACTION_CONNECT_ANDROID);
+				if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+				{
+					intent.putExtra(RemoteAndroidManager.EXTRA_THEME_ID,android.R.style.Theme_Holo_Light_DarkActionBar);
+				}
+				else
+				{
+					intent.putExtra(RemoteAndroidManager.EXTRA_THEME_ID,android.R.style.Theme_Holo_Light_NoActionBar);
+				}
+		    	startActivityForResult(intent, REQUEST_CONNECT_CODE);
+		        return true;
+		    default:
+		        return super.onOptionsItemSelected(item);
+	    }
+		
+	}
 }
